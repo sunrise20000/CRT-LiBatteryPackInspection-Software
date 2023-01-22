5c2320ecfb5a041fb2148db7e7b6987a09843819
DESKTOP-GPE37UV\THINKAPD <tw919@qq.com>
2023-01-22 18:04:05 +0800

    机械手测试程序完成，Blend调试动画没有问题。

--- Crt.UiCore/WpfControlLibrary1/Controls/YamahaRobot600.xaml.cs
+++ Crt.UiCore/WpfControlLibrary1/Controls/YamahaRobot600.xaml.cs
@@ -1,4 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media.Animation;
 
@@ -12,7 +17,7 @@ namespace Crt.UiCore.Controls
 
         public enum Positions
         {
-            System,
+            Standby,
             FeederA,
             FeederB,
             Station1A,
@@ -25,13 +30,135 @@ namespace Crt.UiCore.Controls
 
         private readonly Dictionary<Positions, Storyboard> _storyboards;
 
-        private Positions _lastPosition;
+        private Positions? _lastPosition;
 
         #endregion
 
         public YamahaRobot600()
         {
             InitializeComponent();
+
+            var isDesignMode = DesignerProperties.GetIsInDesignMode(this);
+            if (isDesignMode)
+                return;
+
+            // 每个点位的动画
+            _storyboards = new Dictionary<Positions, Storyboard>
+            {
+                { Positions.Standby, FindResource("ToStandbyStoryboard") as Storyboard },
+                { Positions.FeederA, FindResource("ToFeederBStoryboard") as Storyboard },
+                { Positions.FeederB, FindResource("ToFeederAStoryboard") as Storyboard },
+                { Positions.Station1A, FindResource("ToStation1BStoryboard") as Storyboard },
+                { Positions.Station1B, FindResource("ToStation1AStoryboard") as Storyboard }
+            };
+
+            foreach (var storyboard in _storyboards.Values)
+            {
+                storyboard.Completed += StoryboardOnCompleted;
+            }
+
+            _lastPosition = null;
+
+            AnimationBusy = false;
+
+            StoryboardMonitor();
         }
+
+        #region Properties
+
+        private bool _animationBusy = false;
+
+        private bool AnimationBusy
+        {
+            get
+            {
+                lock (_animationLock)
+                {
+                    return _animationBusy;
+                }
+            }
+            set
+            {
+                lock (_animationLock)
+                {
+                    _animationBusy = value;
+                }
+            }
+        }
+
+        #endregion
+
+        #region Dependency Properties
+
+        public static readonly DependencyProperty CurrentPositionProperty = DependencyProperty.Register(
+            nameof(CurrentPosition), typeof(Positions), typeof(YamahaRobot600), new PropertyMetadata(default(Positions)));
+
+        /// <summary>
+        /// 设置或返回机械手工具末端目前所在的位置。
+        /// </summary>
+        public Positions CurrentPosition
+        {
+            get => (Positions)GetValue(CurrentPositionProperty);
+            set => SetValue(CurrentPositionProperty, value);
+        }
+
+        public static readonly DependencyProperty HasBatteryProperty = DependencyProperty.Register(
+            nameof(HasBattery), typeof(bool), typeof(YamahaRobot600), new PropertyMetadata(default(bool)));
+
+        /// <summary>
+        /// 设置或返回机械手上是否有电池。
+        /// </summary>
+        public bool HasBattery
+        {
+            get => (bool)GetValue(HasBatteryProperty);
+            set => SetValue(HasBatteryProperty, value);
+        }
+
+        #endregion
+
+        #region Methods
+
+        private void StoryboardMonitor()
+        {
+            Task.Run(async () =>
+            {
+                while (true)
+                {
+                    try
+                    {
+                        var currPos = Dispatcher.Invoke(() => (Positions)GetValue(CurrentPositionProperty));
+
+                        if (_lastPosition != currPos && !AnimationBusy)
+                        {
+                            Debug.WriteLine($"_lastPosition: {_lastPosition}, CurrentPosition: {currPos}");
+                            var sb = _storyboards.ContainsKey(currPos) ? _storyboards[currPos] : null;
+
+                            Debug.WriteLine(_storyboards[currPos]);
+                            AnimationBusy = true;
+                            Dispatcher.Invoke(() => sb?.Begin());
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Debug.Write(ex);
+                    }
+
+
+                    await Task.Delay(10);
+                }
+            });
+        }
+
+        #endregion
+
+        #region Events
+
+        private void StoryboardOnCompleted(object sender, EventArgs e)
+        {
+            _lastPosition = CurrentPosition;
+            AnimationBusy = false;
+        }
+
+        #endregion
     }
 }
--- UserControlDev/MainWindow.xaml.cs
+++ UserControlDev/MainWindow.xaml.cs
@@ -17,18 +17,55 @@ namespace UserControlDev
 
             Task.Run(async () =>
             {
-                await Task.Delay(3000);
+                await Task.Delay(1000);
 
                 while (true)
                 {
                     try
                     {
-                        Debug.WriteLine($"Move to {YamahaRobot.Positions.System}");
-                        Invoke(()=> robotYamaha.CurrentPosition = YamahaRobot.Positions.System);
+                        Debug.WriteLine($"Move to {YamahaRobot600.Positions.Standby}");
+                        Invoke(()=>
+                        {
+                            robotYamaha.CurrentPosition = YamahaRobot600.Positions.Standby;
+                            robotYamaha.HasBattery = false;
+                        });
+                        
                         await Task.Delay(500);
 
-                        Debug.WriteLine($"Move to {YamahaRobot.Positions.FeederA}");
-                        Invoke(() => robotYamaha.CurrentPosition = YamahaRobot.Positions.FeederA);
+                        Debug.WriteLine($"Move to {YamahaRobot600.Positions.FeederA}");
+                        Invoke(() => robotYamaha.CurrentPosition = YamahaRobot600.Positions.FeederA);
+                        await Task.Delay(500);
+                        Invoke(()=> robotYamaha.HasBattery = true);
+                        await Task.Delay(500);
+
+                        Debug.WriteLine($"Move to {YamahaRobot600.Positions.Standby}");
+                        Invoke(() => robotYamaha.CurrentPosition = YamahaRobot600.Positions.Standby);
+                        await Task.Delay(500);
+
+                        Debug.WriteLine($"Move to {YamahaRobot600.Positions.Station1A}");
+                        Invoke(() => robotYamaha.CurrentPosition = YamahaRobot600.Positions.Station1A);
+                        await Task.Delay(500);
+                        Invoke(() => robotYamaha.HasBattery = false);
+                        await Task.Delay(500);
+
+                        Debug.WriteLine($"Move to {YamahaRobot600.Positions.Standby}");
+                        Invoke(() => robotYamaha.CurrentPosition = YamahaRobot600.Positions.Standby);
+                        await Task.Delay(500);
+
+                        Debug.WriteLine($"Move to {YamahaRobot600.Positions.FeederB}");
+                        Invoke(() => robotYamaha.CurrentPosition = YamahaRobot600.Positions.FeederB);
+                        await Task.Delay(500);
+                        Invoke(() => robotYamaha.HasBattery = true);
+                        await Task.Delay(500);
+
+                        Debug.WriteLine($"Move to {YamahaRobot600.Positions.Standby}");
+                        Invoke(() => robotYamaha.CurrentPosition = YamahaRobot600.Positions.Standby);
+                        await Task.Delay(500);
+
+                        Debug.WriteLine($"Move to {YamahaRobot600.Positions.Station1B}");
+                        Invoke(() => robotYamaha.CurrentPosition = YamahaRobot600.Positions.Station1B);
+                        await Task.Delay(500);
+                        Invoke(() => robotYamaha.HasBattery = false);
                         await Task.Delay(500);
                     }
                     catch (Exception e)
@@ -39,7 +76,7 @@ namespace UserControlDev
                 }
             });
         }
-
+        
         private void Invoke(Action action)
         {
             Dispatcher.Invoke(action);
@@ -47,14 +84,14 @@ namespace UserControlDev
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            Debug.WriteLine($"Move to {YamahaRobot.Positions.System}");
-            robotYamaha.CurrentPosition = YamahaRobot.Positions.System;
+            Debug.WriteLine($"Move to {YamahaRobot600.Positions.Standby}");
+            robotYamaha.CurrentPosition = YamahaRobot600.Positions.Standby;
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-            Debug.WriteLine($"Move to {YamahaRobot.Positions.FeederA}");
-            robotYamaha.CurrentPosition = YamahaRobot.Positions.FeederA;
+            Debug.WriteLine($"Move to {YamahaRobot600.Positions.FeederA}");
+            robotYamaha.CurrentPosition = YamahaRobot600.Positions.FeederA;
         }
     }
 }
