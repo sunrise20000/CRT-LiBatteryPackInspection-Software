1a88938ed618a4c14f1b9f36eeada71ea595fdc9
DESKTOP-GPE37UV\THINKAPD <tw919@qq.com>
2023-01-29 12:51:23 +0800

    新增S1到S2移栽控件。

---
+++ Crt.UiCore/WpfControlLibrary1/Controls/TransporterS1ToS2.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Windows.Media.Animation;
+using Crt.UiCore.Controls.Base;
+
+namespace Crt.UiCore.Controls
+{
+    /// <summary>
+    /// Interaction logic for TransporterS1ToS2.xaml
+    /// </summary>
+    public partial class TransporterS1ToS2 : BatteryCarrierBase
+    {
+        public enum Positions
+        {
+            Standby,
+            End
+        }
+        
+        public TransporterS1ToS2()
+        {
+            InitializeComponent();
+        }
+
+        protected override void BuildStoryboard()
+        {
+            Storyboards = new Dictionary<int, Storyboard>
+            {
+                { (int)Positions.Standby, FindResource("ToStandbyStoryboard") as Storyboard },
+                { (int)Positions.End, FindResource("ToEndStoryboard") as Storyboard }
+            };
+        }
+    }
+}
